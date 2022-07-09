f0d9abb174602932c29ed0d114ce76122b5501f1
Bryan <viedoura@gmail.com>
2022-07-09 11:59:11 +0700

    Trying history state javascript

--- static/script.js
+++ static/script.js
@@ -1,6 +1,13 @@
+window.onpopstate = function(event) {
+    show(event.state.id)
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.nav-button').forEach(button => {
-        button.onclick = () => show(button.dataset.id);
+        button.onclick = () => {
+            show(button.dataset.id);
+            history.pushState({id: button.dataset.id}, '', `${button.dataset.id}`)
+        }
     })
 })
 
